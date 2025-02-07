525a3bff246b3dbddb9a30bbe8398a2fb7289068
mystica2000 <mysticainf@gmail.com>
2025-02-07 16:53:18 +0530

    update full square, refactor fix

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,41 +3,64 @@ import { useState } from "react";
 export function Board() {
 
     const [edges, setEdges] = useState(new Set());
+    const [hoveredEdges, setHoveredEdges] = useState<string[]>([]);
 
     const handleEdgeClick = (row: number, col: number, edge: "top" | "bottom" | "left" | "right") => {
         console.log("HANDLING EDGE CLICKEYYY - ", row, col, edge);
 
+        const edgeKey = `${row},${col},${edge}`;
+        const adjacentEdge = getAdjacentEdge(row, col, edge);
+        const edgeKeys = adjacentEdge ? [edgeKey, adjacentEdge] : [edgeKey];
 
-        let edgeKey: string = "";
-
-        if (edge == "top") {
-            edgeKey = `${row},${col},top`;
-        } else if (edge == "bottom") {
-            if (row == 7) {
-                edgeKey = `${row},${col},top`;
-            } else {
-                edgeKey = `${row + 1},${col},top`;
-            }
+        if (adjacentEdge != null) {
+            edgeKeys.push(adjacentEdge);
         }
 
-        if (edge == "left") {
-            edgeKey = `${row},${col},left`;
-        } else if (edge == "right") {
-            if (col == 7) {
-                edgeKey = `${row},${col},left`;
-            } else {
-                edgeKey = `${row},${col + 1},left`;
-            }
-        }
-
-        if (edges.has(edgeKey)) {
-            console.log("OHHHH")
+        if (edgeKeys.some((aEdge) => edges.has(aEdge))) {
             return;
-        } else {
-            console.log("OMG NEW");
         }
 
-        setEdges(prev => new Set(prev.add(edgeKey)));
+        setEdges(prev => {
+            const previousSet = new Set(prev);
+            edgeKeys.forEach((aEdge) => previousSet.add(aEdge));
+            return previousSet
+        });
+    }
+
+    const getAdjacentEdge = (row: number, col: number, edge: "top" | "bottom" | "left" | "right"): string | null => {
+        switch (edge) {
+            case "top": {
+                if (row > 0) return `${row - 1},${col},bottom`;
+                break;
+            }
+            case "bottom": {
+                if (row < 7) return `${row + 1},${col},top`;
+                break;
+            }
+            case "left": {
+                if (col > 0) return `${row},${col - 1},right`;
+                break;
+            }
+            case "right": {
+                if (col < 7) return `${row},${col + 1},left`;
+                break;
+            }
+        }
+        return null;
+    };
+
+    const handleMouseEntered = (row: number, col: number, edge: "top" | "bottom" | "left" | "right") => {
+        const edgeKey = `${row},${col},${edge}`;
+        const adjacentEdge = getAdjacentEdge(row, col, edge);
+        setHoveredEdges(adjacentEdge ? [edgeKey, adjacentEdge] : [edgeKey]);
+    }
+
+    const handleMouseLeave = () => {
+        setHoveredEdges([]);
+    };
+
+    const hasFullSquare = (row: number, col: number) => {
+        return edges.has(`${row},${col},top`) && edges.has(`${row},${col},bottom`) && edges.has(`${row},${col},left`) && edges.has(`${row},${col},right`)
     }
 
     return <section className="border-2 border-lime-200">
@@ -46,23 +69,32 @@ export function Board() {
                 <div key={row} className="flex">
                     {Array(8).fill(0).map((_, col) => {
                         return (
-                            <div key={col} className="relative w-20 h-20 border">
+                            <div key={col} className={`relative w-20 h-20 ${hasFullSquare(row, col) ? 'bg-black' : ''}`}>
+
+                                <div className={`absolute inset-0 border-2 pointer-events-none
+                                    ${edges.has(`${row},${col},top`) ? 'border-t-blue-600' : hoveredEdges.includes(`${row},${col},top`) ? 'border-t-blue-200' : 'border-t-lime-200'}
+                                    ${edges.has(`${row},${col},right`) ? 'border-r-blue-600' : hoveredEdges.includes(`${row},${col},right`) ? 'border-r-blue-200' : 'border-r-lime-200'}
+                                    ${edges.has(`${row},${col},bottom`) ? 'border-b-blue-600' : hoveredEdges.includes(`${row},${col},bottom`) ? 'border-b-blue-200' : 'border-b-lime-200'}
+                                    ${edges.has(`${row},${col},left`) ? 'border-l-blue-600' : hoveredEdges.includes(`${row},${col},left`) ? 'border-l-blue-200' : 'border-l-lime-200'}`}
+                                >
+
+                                </div>
 
                                 {/* top */}
-                                <div className={`absolute top-0 left-0 w-full h-0.5 bg-lime-200 cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "top")}>
+                                <div className={`absolute top-0 left-0 w-full h-1 cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "top")} onMouseEnter={() => handleMouseEntered(row, col, "top")} onMouseLeave={handleMouseLeave}>
                                 </div>
 
                                 {/* right */}
-                                <div className={`absolute top-0 right-0 w-0.5 h-full bg-lime-200 cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "right")}></div>
+                                <div className={`absolute top-0 right-0 w-1 h-full cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "right")} onMouseEnter={() => handleMouseEntered(row, col, "right")} onMouseLeave={handleMouseLeave}></div>
 
                                 {/* left */}
-                                <div className={`absolute top-0 left-0 w-0.5 h-full bg-lime-200 cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "left")}>
+                                <div className={`absolute top-0 left-0 w-1 h-full cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "left")} onMouseEnter={() => handleMouseEntered(row, col, "left")} onMouseLeave={handleMouseLeave}>
                                 </div>
 
                                 {/* bottom */}
-                                <div className={`absolute bottom-0 left-0 h-0.5 w-full bg-lime-200 cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "bottom")}>
-
+                                <div className={`absolute bottom-0 left-0 h-1 w-full cursor-pointer`} onClick={(e) => handleEdgeClick(row, col, "bottom")} onMouseEnter={() => handleMouseEntered(row, col, "bottom")} onMouseLeave={handleMouseLeave}>
                                 </div>
+
                             </div>
                         )
                     })}
@@ -71,5 +103,5 @@ export function Board() {
             ))
         }
 
-    </section>
+    </section >
 }
